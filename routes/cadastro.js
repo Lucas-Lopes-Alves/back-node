b9745c518d6decc7f6a84e8ba1309ae9d50722a1
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-02-11 10:07:42 -0300

    .

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -7,4 +7,10 @@ router.post("/cadastro", (req,res)=>{
     res.status(201).json(user)
 })
 
+// __dirname = "C:\Users\LUCASLOPESALVESDESOU\Desktop\back"
+
+router.get("/usuario", (req,res)=>{
+    res.send(JSON.stringify(`{ "nome": lucas } `))
+})
+
 export default router
