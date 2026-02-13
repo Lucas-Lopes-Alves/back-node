c5e551551f78075b50b2696dc0a29c83279032a3
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-02-13 08:05:18 -0300

    .

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
-import mysql from "mysql2/promises"
+import mysql from "mysql2/promise.js"
 
 const connection = mysql.createPool({
   host: "192.168.56.1",
   user: "acesso",
   password: "150322Em$",
+  database: "app_db",
   port: "3306"
 })
 
--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import connection from './db.js'
+import connection from '../db.js'
 
 const router = express.Router()
 
 function cadastrar(req,res){
     const user = req.body
-    connection
-    res.status(202).json(user)
+    connection.query("INSERT INTO usuarios(nome) VALUES(?)", [req.body.nome])
+    res.status(201).json(user)
     console.log(JSON.stringify(user,null,4))
 }
 
